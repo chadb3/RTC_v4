7c284616066973ef316a03741580fafc559e7769
Chad <chadb78999@gmail.com>
2026-09-22 01:19:37 -0500

    Chapter 1 work. Made my tuple immutable. implimented operators. more unit tests.

--- RTC_v4/RTTuple.cs
+++ RTC_v4/RTTuple.cs
@@ -10,12 +10,13 @@ namespace RTC_v4
     /// </summary>
     public struct RTTuple
     {
-        public double x { get; set; }
-        public double y { get; set; }
-        public double z { get; set; }
-        public double w { get; set; }
+        public double x {get;}
+        public double y {get;}
+        public double z {get;}
+        public double w {get;}
 
-        public RTTuple(double x, double y, double z, double w)
+
+        public  RTTuple(double x, double y, double z, double w)
         {
             this.x = x;
             this.y = y;
@@ -38,9 +39,49 @@ namespace RTC_v4
             return new RTTuple(r, g, b, 0);
         }
 
-        // Code based off AI Answer
-        // I know this code is largley not used after unit tests in chapter 1.
+        public static bool operator ==(RTTuple a, RTTuple b)
+        {
+            return Math.Abs(a.x - b.x) < 0.00001 &&
+                   Math.Abs(a.y - b.y) < 0.00001 &&
+                   Math.Abs(a.z - b.z) < 0.00001 &&
+                   Math.Abs(a.w - b.w) < 0.00001;
+        }
+
+        public static bool operator !=(RTTuple a, RTTuple b)
+        {
+            return !(a == b);
+        }
+
+        /// <summary>
+        /// Checks if two RTTuple instances are equal based on their components.
+        /// Notes: AI help (9/21/2026): helped with adding readonly and adding the question mark to clear green squggle lines. I think I would have tried readonly as it was in the highlight message, but the question mark was new to me.
+        /// The ? on object? means the parameter is allowed to be null, matching the base Equals(object?) contract so the override doesn’t illegally narrow its nullability. Even though a struct itself can never be null, the Equals method must still accept a nullable object because callers are allowed to pass null into the base API.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public readonly override bool Equals(object? obj)
+        {
+            if (obj is RTTuple other)
+            {
+                return this == other;
+            }
+            return false;
+        }
+        /// <summary>
+        /// Generates a hash code for the RTTuple instance based on its components.
+        /// Tab generated
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(x, y, z, w);
+        }
+
+        // Code based off AI Answer (9/21/2026)
+        // I know both checks are largely not used after unit tests in chapter 1. 
+        // checks if a tuple is a point
         public bool IsPoint=>Math.Abs(w - 1.0) < 0.00001;
-        public bool IsVector => Math.Abs(w-0.0) < 0.00001;
+        // checks if a tuple is a vector
+        public bool IsVector => Math.Abs(w) < 0.00001;
     }
 }
--- Ray_Tracer_Tests/UnitTest1.cs
+++ Ray_Tracer_Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
-﻿using System.IO.Pipelines;
+﻿using System.Drawing;
+using System.IO.Pipelines;
 using RTC_v4;
 
 namespace Ray_Tracer_Tests
@@ -32,7 +33,7 @@ namespace Ray_Tracer_Tests
         [Fact]
         public void Test2()
         {
-            var a = new RTTuple(4.3,-4.2, 3.1, 0.0);
+            var a = new RTTuple(4.3, -4.2, 3.1, 0.0);
             Assert.Equal(4.3, a.x);
             Assert.Equal(-4.2, a.y);
             Assert.Equal(3.1, a.z);
@@ -40,5 +41,49 @@ namespace Ray_Tracer_Tests
             Assert.False(a.IsPoint);
             Assert.True(a.IsVector);
         }
+
+        [Fact]
+        public void Test3()
+        {
+            RTTuple p = RTTuple.point(4, -4, 3);
+            var tuple = new RTTuple(4, -4, 3, 1);
+            Assert.Equal(tuple, p);
+            Assert.True(p.IsPoint);
+            Assert.False(p.IsVector);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            RTTuple v = RTTuple.vector(4, -4, 3);
+            var tuple = new RTTuple(4, -4, 3, 0);
+            Assert.Equal(tuple, v);
+            Assert.True(v.IsVector);
+            Assert.False(v.IsPoint);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            RTTuple v = RTTuple.vector(4, -4, 3);
+            var tuple = new RTTuple(4, -4, 3, 0);
+            Assert.True(v.Equals(tuple));
+        }
+        [Fact]
+        public void Test6()
+        {
+            object t1 = new RTTuple(4, -4, 3, 0);
+            object t2 = new RTTuple(0, 3, -4, 4);
+            Assert.False(t1.GetHashCode() == t2.GetHashCode());
+        }
+        //[Fact]
+        /// Test to check mutability of RTTuple. in my research, it should be immutable. It is now commented out as now it is read only / immutable.
+        /*public void Test7()
+
+        {
+            RTTuple T1 = new RTTuple(1, 2, 3, 0);
+            T1.w = 1;
+            Assert.True(T1.IsPoint);
+        }*/
     }
 }
